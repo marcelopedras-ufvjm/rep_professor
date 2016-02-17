ed7546e095968bbe18c870104035ff4a87eb2d96
marcelo <marcelo.braulio.si@gmail.com>
2016-02-17 20:36:04 -0200

    Adicionado interceptor para adicionar informações ao header com token e outras informações de autenticação durante request.

--- public/app/app.js
+++ public/app/app.js
@@ -13,4 +13,25 @@ var mainApp = angular.module('rep_professor',['ng-token-auth', 'ngRoute'])
                 controller: 'LoginController',
                 controllerAs : 'myCtrl'
             });
-}]);
+}])
+.config(function ($provide, $httpProvider) {
+
+    // Intercept http calls.
+    $provide.factory('AuthInterceptor', function ($q) {
+        return {
+            // On request success
+            request: function (config) {
+                var ls = JSON.parse(localStorage.getItem("auth_headers"));
+                for (var attrname in  ls) {
+                    config.headers[attrname] = ls[attrname];
+                }
+                return config || $q.when(config);
+            }
+        };
+    });
+
+    // Add the interceptor to the $httpProvider.
+
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
+
